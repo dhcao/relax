b3d60187bbfadf403372271318e6d445a1d7d1b6
K <dhcao1112@163.com>
2018-03-26 17:26:19 +0800

    first time commit my project ..by dhcao20180326

--- src/main/java/com/xiawan/SpringBootApplication.java
+++ src/main/java/com/xiawan/SpringBootApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 /**
  * Created by dhcao. on 2018/3/26.
  */
-@org.springframework.boot.autoconfigure.SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@org.springframework.boot.autoconfigure.SpringBootApplication
 public class SpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApplication.class,args);
